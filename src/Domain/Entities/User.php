ca7aee1967a3162de05a8f1820ac2f7bf89977a6
jhondev123 <jhonattan.curtarelli@outlook.com>
2024-11-20 23:25:25 -0300

    feat: criando a Entity User

---
+++ src/Domain/Entities/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jhonattan\PizzariaDelivery\Domain\Entities;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'products')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    public string $id;
+
+    #[ORM\Column(type: 'string')]
+    public string $name;
+
+}
